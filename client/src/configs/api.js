9914846ebf4db8401883e90f10f762140f42ef05
Rajatsoni896 <rajatsoni896@gmail.com>
2026-01-18 13:51:06 +0530

    udate the url in frontend

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
+  //baseURL: "https://ai-resume-builder-a3v8.onrender.com"
+  baseURL: "https://ai-resume-builder-a3v8.onrender.com"
+
+
 })
 
 export default api
