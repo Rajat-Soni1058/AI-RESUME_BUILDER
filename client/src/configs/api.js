061adf022051c66e3c844b659d2598d105bf68dc
Rajatsoni896 <rajatsoni896@gmail.com>
2026-01-18 14:06:27 +0530

    update the base url in env

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: "https://ai-resume-builder-a3v8.onrender.com"
-  baseURL: "https://ai-resume-builder-a3v8.onrender.com"
+  baseURL: import.meta.env.VITE_API_BASE_URL
 
 
 })
